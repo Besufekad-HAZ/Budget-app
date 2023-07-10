1ce169d674436ac932fe2a43828aaa1a1383d414
Besufekad-HAZ <besurye@gmail.com>
2023-07-10 17:22:51 +0300

    Add user, catagories and payment controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  get 'payments/index'
+  get 'payments/show'
+  get 'payments/new'
+  get 'payments/create'
+  get 'payments/edit'
+  get 'payments/update'
+  get 'payments/destroy'
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/create'
+  get 'categories/edit'
+  get 'categories/update'
+  get 'categories/destroy'
+  get 'user/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
