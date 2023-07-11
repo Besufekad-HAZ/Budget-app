98ca9b3558b26ad377dea3007caaf9c0ffb23e2b
Besufekad-HAZ <besurye@gmail.com>
2023-07-11 16:12:34 +0300

    Update and fix the catagories tests for requests

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe '/', type: :request do
-  context 'GET /' do
-    it 'renders a successful response' do
-      get root_path
-      expect(response).to have_http_status(200)
+RSpec.describe 'Categories', type: :request do
+  describe 'GET /categories' do
+    it 'redirects to sign-in if user is not logged in' do
+      get categories_path
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 end
