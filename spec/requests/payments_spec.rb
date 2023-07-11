09a983012bc623c7a8976f41189d7de53b19abe7
Besufekad-HAZ <besurye@gmail.com>
2023-07-11 16:09:17 +0300

    Fix rubocop linter error

--- spec/requests/payments_spec.rb
+++ spec/requests/payments_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "Payments", type: :request do
-  describe "GET /payments" do
-    it "works! (now write some real specs)" do
+RSpec.describe 'Payments', type: :request do
+  describe 'GET /payments' do
+    it 'works! (now write some real specs)' do
       get payments_index_path
       expect(response).to have_http_status(200)
     end
