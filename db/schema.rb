dc3dbc8c7b6f4882bcc9b71ac95199147dfe6468
Besufekad-HAZ <besurye@gmail.com>
2023-07-13 11:23:33 +0300

    Update payments migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_12_172527) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_13_081136) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,7 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_12_172527) do
 
   create_table "payments", force: :cascade do |t|
     t.string "name"
-    t.bigint "amount"
+    t.decimal "amount"
     t.bigint "category_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
