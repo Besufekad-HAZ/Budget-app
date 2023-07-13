71f5cc076f3572bc820f46adf08a5789b8af4f53
Besufekad-HAZ <besurye@gmail.com>
2023-07-13 11:35:33 +0300

    Update payment model to accepts decimal payments values

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,5 +3,5 @@ class Payment < ApplicationRecord
   has_many :category, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 25 }
-  validates :amount, presence: true, numericality: { greater_than: 0, only_integer: true }
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
